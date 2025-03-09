50f0ef1b56e4e9ca15fb5e01fdbf6d743ac1e2e7
Shaikat0079 <baruashaikat00790@gmail.com>
2025-03-09 14:32:45 +0930

    Constructor

--- class.js
+++ class.js
@@ -1,14 +1,18 @@
 class Product {
-    // constructor(parameters) {
-        
-    // }
-    malik = "Alpha"
+    constructor(pName,price,brand,malik) {
+        this.pName = pName
+        this.price = price
+        this.brand = brand
+        this.malik = malik
+        console.log(pName,price,brand)
+    }
+    // malik = "Alpha"
     details(){
-        console.log(`I'm Details from`)
+        console.log(`I'm Details from ${this.brand}`)
     }
     
 }
 
-const iPhone = new Product();
+const iPhone = new Product('iPhone','1234','Apple',"Steve");
 console.log(iPhone)
 iPhone.details()
