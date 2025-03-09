6a287e5f6779e9fe7ebbff86b89afb8dc7672838
Shaikat0079 <baruashaikat00790@gmail.com>
2025-03-09 14:10:27 +0930

    Added necessary files

---
+++ class.js
@@ -0,0 +1,14 @@
+class Product {
+    // constructor(parameters) {
+        
+    // }
+    malik = "Alpha"
+    details(){
+        console.log(`I'm Details from`)
+    }
+    
+}
+
+const iPhone = new Product();
+console.log(iPhone)
+iPhone.details()
